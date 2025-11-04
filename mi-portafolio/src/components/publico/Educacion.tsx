34e7a1f209357a21661e2e04d0080d036ae18f0a
criscrman <70421896+criscrman@users.noreply.github.com>
2025-11-04 02:27:08 -0500

    Se crea un primer borrador de la pagina Eduacion

--- mi-portafolio/src/components/publico/Educacion.tsx
+++ mi-portafolio/src/components/publico/Educacion.tsx
@@ -1,14 +1,56 @@
-
+import './Educacion.css'
 
 export const Educacion = () => {
   return (
       
+    <main>
       <h1>Educación Pepe</h1>
-      
-      
-      
 
 
+      <div className="secuContainer">
+            <h2 className='h2Educacion'>Educación Secundaria</h2>
+            <p>Bachiller Acádemico Prom 2014. Egresado de la Institución Educativa Ciudadela Cuba.</p>
+
+            <div className="ImgConterner">
+
+            <img className="Imgsecu" src="https://i.postimg.cc/3x3QzSGq/ciudadela-cuba.jpg" alt="Institución Educativa Ciudadela Cuba"  />
+            <img className="Imgsecu" src="https://i.postimg.cc/3x3QzSGq/ciudadela-cuba.jpg" alt="Institución Educativa Ciudadela Cuba"  />
+            </div>
+
+      </div>
+
+      <div className="UniContainer">
+          <h2 className='h2Educacion'>Educación Universitaria</h2>
+          <h3>Tecnología en Sistemas de Comunicaciones Inalámbricas.</h3>
+          <p></p>
+          <ul>
+            <li>Institución: Universidad Nacional Abierta y a Distancia.</li>
+            <li>Año de Inicio: 2018</li>
+            <li>Año de Culminación: 2021</li>
+          </ul>
+          <h4>Resumen de la Carrera:</h4>
+          <p>El programa de Tecnología en Sistemas de Comunicaciones Inalámbricas forma profesionales capacitados para diseñar, implementar y gestionar redes de comunicación inalámbrica, utilizando tecnologías avanzadas y adaptándose a las necesidades del mercado actual.</p>
+          <p>Logrando traer conocimientos actualizados y relevantes para el desarrollo de soluciones innovadoras en el campo de las telecomunicaciones. Además de:</p>
+          <ul>
+            <li>Implementación de la red de acceso de telecomunicaciones inalámbrica con base en un diseño establecido.</li>
+            <li>Implementar tecnologías emergentes en telecomunicaciones.</li>
+            <li>Gestionar proyectos de infraestructura de redes inalámbricas.</li>
+            <li>Aplicar normativas y estándares internacionales en telecomunicaciones.</li>
+            <li>Gestión y monitoreo de redes y servicios de telecomunicaciones</li>
+          </ul>
+          <h3>Ingeniería de Sistemas.</h3>
+          <p></p>
+          
+          <ul>
+            <li>Institución: Universidad Nacional Abierta y a Distancia.</li>
+            <li>Año de Inicio: 2021</li>
+            <li>Año de Culminación: 2025</li>
+            <li>Programa: Ingeniería de Sistemas</li>
+          </ul>
+
+      </div>
+
+    </main>
   )
 }
 

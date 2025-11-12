0318063f721a7c964b56dd11b93f28e690e20a71
criscrman <70421896+criscrman@users.noreply.github.com>
2025-11-12 02:23:16 -0500

    Se crean los logos de los Skills y se pone una fuente mejor

--- mi-portafolio/src/components/publico/Skills.tsx
+++ mi-portafolio/src/components/publico/Skills.tsx
@@ -1,5 +1,22 @@
 import './Skills.css'
 import logoPython from '../img/python-logo.png'
+import logoPHP from '../img/PHP_Logo.jpg'
+import HTMLLogo from '../img/logo_HTML5.webp'
+import CSSLogo from '../img/logo_CSS.webp'
+import JSLogo from '../img/logo_JS.png'
+import logoTailwind from '../img/tailwindcss_logo.png'
+import logoSQL from '../img/MySQL_Logo.png'
+import logoNoSQL from '../img/noSQL_Logo.webp'
+import logoExcel from '../img/Excel-Logo.jpg'
+import logoPowerBI from '../img/powerBi_Logo.png'
+import logoTableau from '../img/tablueau_Logo.png'
+import logoGoogleColab from '../img/GoogleColab_Logo.png'
+import logoPandas from '../img/Pandas_Logo.png'
+import logoNumPy from '../img/Numpy_Logo.png'
+import logoMatplotlib from '../img/MathPlotlib_Logo.webp'
+import logoReactNative from '../img/React_Logo.webp'
+import logoGit from '../img/GIT_Logo.webp'
+import logoGitHub from '../img/GitHub-Logo.png'
 
 export const Skills = () => {
   return (
@@ -21,49 +38,47 @@ export const Skills = () => {
             <li>Desarrollo web (HTML, CSS, JavaScript, Tailwind, React)</li>
             <li>Programación en Python y PHP.
               <div className='logosSkillsConteiners'>
+              <img className="logosSkills" src={HTMLLogo} alt="Logo de HTML"  />
+              <img className="logosSkills " src={CSSLogo} alt="Logo de CSS"  />
+              <img className="logosSkills" src={JSLogo} alt="Logo de JavaScript"  />
+              <img className="logosSkills" src={logoTailwind} alt="Logo de Tailwind"  />
               <img className="logosSkills" src={logoPython} alt="Logo de Python"  />
-              <img className="logosSkills" src={logoPython} alt="Logo de PHP"  />
-              <img className="logosSkills" src={logoPython} alt="Logo de React"  />
-              <img className="logosSkills" src={logoPython} alt="Logo de Tailwind"  />
-              </div>
+              <img className="logosSkills" src={logoPHP} alt="Logo de PHP"  />  
+              <img className="logosSkills" src={logoReactNative} alt="Logo de React Native"  />            </div>
             </li>
 
-            <li>Gestión de bases de datos (SQL, NoSQL)
+            <li>Gestión de bases de datos (SQL, NoSQL).
               <div className='logosSkillsConteiners'>
-              <img className="logosSkills" src={logoPython} alt="Logo de SQL"  />
-              <img className="logosSkills" src={logoPython} alt="Logo de NoSQL"  />
+              <img className="logosSkills" src={logoSQL} alt="Logo de SQL"  />
+              <img className="logosSkills" src={logoNoSQL} alt="Logo de NoSQL"  />
               
               </div>
             </li>
 
-            <li>Análisis de datos(Excel, Power BI)
+            <li>Análisis de datos: Exploración y limpieza de datos(Excel, Power BI).
               <div className='logosSkillsConteiners'>
-              <img className="logosSkills" src={logoPython} alt="Logo de Excel"  />
-              <img className="logosSkills" src={logoPython} alt="Logo de Power BI"  />
+              <img className="logosSkills" src={logoExcel} alt="Logo de Excel"  />
+              <img className="logosSkills" src={logoPowerBI} alt="Logo de Power BI"  />
               </div>
             </li>
-            <li>Transformación y visualización de datos (Tableau, Power BI, Google Colab)
+            <li>Transformación y visualización de datos (Tableau, Power BI, Google Colab).
               <div className='logosSkillsConteiners'>
-              <img className="logosSkills" src={logoPython} alt="Logo de Tableau"  />
-              <img className="logosSkills" src={logoPython} alt="Logo de Power BI"  />
-              <img className="logosSkills" src={logoPython} alt="Logo de Google Colab"  />
+              <img className="logosSkills" src={logoTableau} alt="Logo de Tableau"  />
+              <img className="logosSkills" src={logoPowerBI} alt="Logo de Power BI"  />
+              <img className="logosSkills" src={logoGoogleColab} alt="Logo de Google Colab"  />
               </div>
             </li>
-            <li>Librerias para la ciencia de datos (Pandas, NumPy, Matplotlib)
+            <li>Librerias para la ciencia de datos (Pandas, NumPy, Matplotlib).
               <div className='logosSkillsConteiners'>
-              <img className="logosSkills" src={logoPython} alt="Logo de Pandas"  />
-              <img className="logosSkills" src={logoPython} alt="Logo de NumPy"  />
-              <img className="logosSkills" src={logoPython} alt="Logo de Matplotlib"  />
+              <img className="logosSkills" src={logoPandas} alt="Logo de Pandas"  />
+              <img className="logosSkills" src={logoNumPy} alt="Logo de NumPy"  />
+              <img className="logosSkills" src={logoMatplotlib} alt="Logo de Matplotlib"  />
               </div>
             </li>
-            <li>Desarrollo de páginas web básicas (React Native)
+            <li>Control de versiones (Git y GitHub).
               <div className='logosSkillsConteiners'>
-              <img className="logosSkills" src={logoPython} alt="Logo de React Native"  />
-              </div>
-            </li>
-            <li>Control de versiones (Git)
-              <div className='logosSkillsConteiners'>
-              <img className="logosSkills" src={logoPython} alt="Logo de Git"  />
+              <img className="logosSkills" src={logoGit} alt="Logo de Git"  />
+              <img className="logosSkills" src={logoGitHub} alt="Logo de GitHub"  />
               </div>
             </li>
           </ul>
